4f46bf9b166431c1607bc77178b19a2addf3fad0
mirikosmale <mirikosmale@github.com>
2025-04-16 16:51:09 +0300

    amsr

--- start_amsr_svalbard_timeseries.sh
+++ start_amsr_svalbard_timeseries.sh
@@ -19,25 +19,25 @@
 source set_system.sh
 source /data/kosmale/venv/bin/activate
 
-# amsr_svalbard_timeseries.py 2 20230801 20240730 Svalbardcentre
-# amsr_svalbard_timeseries.py 2 20240801 20250730 Svalbardcentre
-# amsr_svalbard_timeseries.py 2 20230401 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 2 20230801 20240730 Svalbardcentre
+amsr_svalbard_timeseries.py 2 20240801 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 2 20230401 20250730 Svalbardcentre
 
-# amsr_svalbard_timeseries.py 1 20230801 20240730 Svalbardcentre
-# amsr_svalbard_timeseries.py 1 20240801 20250730 Svalbardcentre
-# amsr_svalbard_timeseries.py 1 20230401 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 1 20230801 20240730 Svalbardcentre
+amsr_svalbard_timeseries.py 1 20240801 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 1 20230401 20250730 Svalbardcentre
 
-# amsr_svalbard_timeseries.py 4 20230801 20240730 Svalbardcentre
-# amsr_svalbard_timeseries.py 4 20240801 20250730 Svalbardcentre
-# amsr_svalbard_timeseries.py 4 20230401 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 4 20230801 20240730 Svalbardcentre
+amsr_svalbard_timeseries.py 4 20240801 20250730 Svalbardcentre
+amsr_svalbard_timeseries.py 4 20230401 20250730 Svalbardcentre
 
-# amsr_svalbard_timeseries.py 2 20230801 20240730 Larsbren
-# amsr_svalbard_timeseries.py 2 20240801 20250730 Larsbren
-# amsr_svalbard_timeseries.py 2 20230401 20250730 Larsbren
+amsr_svalbard_timeseries.py 2 20230801 20240730 Larsbren
+amsr_svalbard_timeseries.py 2 20240801 20250730 Larsbren
+amsr_svalbard_timeseries.py 2 20230401 20250730 Larsbren
 
-# amsr_svalbard_timeseries.py 2 20230801 20240730 Longyearbreen
-# amsr_svalbard_timeseries.py 2 20240801 20250730 Longyearbreen
-# amsr_svalbard_timeseries.py 2 20230401 20250730 Longyearbreen
+amsr_svalbard_timeseries.py 2 20230801 20240730 Longyearbreen
+amsr_svalbard_timeseries.py 2 20240801 20250730 Longyearbreen
+amsr_svalbard_timeseries.py 2 20230401 20250730 Longyearbreen
 
 amsr_svalbard_timeseries.py 4 20230801 20240730 Larsbren
 amsr_svalbard_timeseries.py 4 20240801 20250730 Larsbren
